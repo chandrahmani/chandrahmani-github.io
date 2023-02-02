68818b8c46a44eda02eee003e159a614f0d319ed
Your Name <chanrhamni>
2023-02-02 11:52:31 +0530

    update Profile

---
+++ chandrahmani/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import './index.css'
+
+
+const Profile = () => {
+    return (
+        <>
+            <main className="main-wrapper">
+                <section className="profile-wrapper">
+                    <div class="logo-wrapper">
+                        <img src="https://raw.githubusercontent.com/chandrahmani/chandrahmani.github.io/develop/chand-img.jpeg" />
+                        <h2>Chand Rahmani</h2>
+                        <h3> In ♥️ With Coding, 📷 Photography and Travelling and Cricket</h3>
+                        <h3>Specially ! HTML, Cascading Style Sheets, JavaScript</h3>
+                    </div>
+                    <div className="text-wrapper">
+                        <p>I am a HTML Developer</p>
+                        <p>My Name is Chand I Am In 12th Class I Like Programming</p>
+                    </div>
+
+                    <div className="button button-wrapper">
+                        <button><a href="">click more</a></button>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Profile;
