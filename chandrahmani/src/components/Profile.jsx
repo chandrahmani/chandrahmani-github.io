da407c85a7f3e6f6663374235d7fd982e540ef21
Your Name <chanrhamni>
2023-02-28 11:04:40 +0530

    update profile and add skill

--- chandrahmani/src/components/Profile.jsx
+++ chandrahmani/src/components/Profile.jsx
@@ -1,22 +1,39 @@
+import { Moon, SunDim } from 'phosphor-react';
+import { useState } from 'react';
 import './index.css'
 
 
+
 const Profile = () => {
+    const [click, setClick] = useState();
+
+    const handelClick = () => {
+        setClick(!click)
+    }
+
     return (
         <>
             <main className="main-wrapper">
+                <div className='theme'>
+                    <div onClick={handelClick}>
+                        {click ? <SunDim size={32} /> : <Moon size={32} />}
+                    </div>
+                </div>
                 <section className="profile-wrapper">
-                    <div class="logo-wrapper">
+                    <div className="logo-wrapper">
                         <img src="https://raw.githubusercontent.com/chandrahmani/chandrahmani.github.io/develop/chand-img.jpeg" />
                         <h2>Chand Rahmani</h2>
                         <h3> In ♥️ With Coding, 📷 Photography and Travelling and Cricket</h3>
                         <ul className='skills'>
                             <li>Specially !</li>
                             <li>HTML</li>
-                            <li>Cascading Style Sheets</li>
+                            <li>CSS 3</li>
                             <li>Styled_Components </li>
                             <li>JavaScript</li>
                             <li> React_js</li>
+                            <li>SASS</li>
+                            <li>Git</li>
+                            <li>Bootstrap</li>
                         </ul>
                     </div>
                     <div className="text-wrapper">
@@ -25,7 +42,7 @@ const Profile = () => {
                     </div>
 
                     <div className="button button-wrapper">
-                        <button><a href="">click more</a></button>
+                        <button type='submit'>click more</button>
                     </div>
                 </section>
             </main>
