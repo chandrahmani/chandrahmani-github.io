969832d505be9640a76e8a1db82f631e1dd71523
Chand Rahmani <chandrahmani254@gmail.com>
2023-12-09 16:03:03 +0530

    remove all file

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Inter } from "next/font/google";
-import Profile from "@/components/profile/Profile";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <Profile />;
-}
